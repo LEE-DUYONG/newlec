cdeed4bd3c2eef52b2bd1f539d8322ed07b7109b
endyd <endyd@DESKTOP-8G4UP84>
2019-10-14 15:12:11 +0900

    index.jsp를 보기위한 컨트롤러 추가

---
+++ newlec/src/com/newlec/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.newlec.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PageController implements Controller {
+
+	@Override
+	public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return "dispatcher:/index.jsp";
+	}
+
+}
