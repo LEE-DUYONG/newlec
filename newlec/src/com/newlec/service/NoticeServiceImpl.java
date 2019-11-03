8ccbf0b259d5cd09b05f71d6dd96e66caefa94b8
endyd <endyd@DESKTOP-8G4UP84>
2019-11-03 12:37:28 +0900

    rownum +- 오류 수정

--- newlec/src/com/newlec/service/NoticeServiceImpl.java
+++ newlec/src/com/newlec/service/NoticeServiceImpl.java
@@ -76,10 +76,10 @@ public class NoticeServiceImpl implements NoticeService {
 		// TODO Auto-generated method stub
 		System.out.println("NoticeServiceImpl.noticeNextTitle");
 		
-		// contentNum의 rownum + 1로 다음 게시글 불러오기(contentNum으로 불러오면 삭제되었을 경우 불러오기 불가능)
+		// contentNum의 rownum - 1로 다음 게시글 불러오기(contentNum으로 불러오면 삭제되었을 경우 불러오기 불가능)
 		NoticeBoardDao noticeBoardDao = new OracleNoticeBoardDao();
 		
-		return noticeBoardDao.getBoardTitle(rownum + 1);
+		return noticeBoardDao.getBoardTitle(rownum - 1);
 	}
 
 	@Override
@@ -87,10 +87,10 @@ public class NoticeServiceImpl implements NoticeService {
 		// TODO Auto-generated method stub
 		System.out.println("NoticeServiceImpl.noticePreviousTitle");
 		
-		// contentNum의 rownum - 1로 이전 게시글 불러오기(contentNum으로 불러오면 삭제되었을 경우 불러오기 불가능)
+		// contentNum의 rownum + 1로 이전 게시글 불러오기(contentNum으로 불러오면 삭제되었을 경우 불러오기 불가능)
 		NoticeBoardDao noticeBoardDao = new OracleNoticeBoardDao();
 		
-		return noticeBoardDao.getBoardTitle(rownum - 1);
+		return noticeBoardDao.getBoardTitle(rownum + 1);
 	}
 
 	@Override
